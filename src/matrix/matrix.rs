b5454e80da2c78f6e0221784fbb4553917c225ba
kaczor6418 <k.kaczynski97@gmail.com>
2021-02-19 23:25:54 +0100

    wip(create-matrix): add create matrix methods

--- src/matrix/matrix.rs
+++ src/matrix/matrix.rs
@@ -1,4 +1,36 @@
-pub struct Matrix {}
+pub struct Matrix {
+    values: Vec<f64>,
+    columns_count: usize,
+    rows_count: usize,
+}
+
+impl Matrix {
+    pub fn new(columns_count: usize, values: Vec<f64>) -> Matrix {
+        return Matrix {
+            rows_count: values.len() / columns_count,
+            values,
+            columns_count,
+        };
+    }
+
+    pub fn new_zeros_matrix(rows_count: usize, columns_count: usize) -> Matrix {
+        return Matrix {
+            rows_count,
+            columns_count,
+            values: vec![0.0; rows_count * columns_count],
+        };
+    }
+
+    pub fn new_identity_matrix(rows_count: usize, columns_count: usize) -> Matrix {
+        let mut matrix = Matrix::new_zeros_matrix(rows_count, columns_count);
+        let mut column_index = 0;
+        for row_index in 0..rows_count {
+            matrix[row_index][column_index] = 1.0;
+            column_index += 1;
+        }
+        return matrix;
+    }
+}
 
 #[cfg(test)]
 mod matrix_test;
