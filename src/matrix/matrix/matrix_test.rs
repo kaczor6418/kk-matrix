06c8f9a8fbd1189967d5b8040d5ceaa2cf569934
kaczor6418 <k.kaczynski97@gmail.com>
2021-02-20 00:26:18 +0100

    test(matrix-subtract): add subtract tests

--- src/matrix/matrix/matrix_test.rs
+++ src/matrix/matrix/matrix_test.rs
@@ -203,3 +203,33 @@ mod add {
         assert_eq!(result_matrix.values, expected_result.values);
     }
 }
+
+mod subtract {
+    use crate::matrix::matrix::Matrix;
+
+    #[test]
+    fn should_subtract_each_element_of_matrix_a_and_matrix_b() {
+        let matrix_a_values = vec![1.0, 2.0, 3.0];
+        let matrix_b_values = vec![3.0, 2.0, 1.0];
+        let expected_result = Matrix::new(3, vec![-2.0, 0.0, 2.0]);
+        let matrix_a = Matrix::new(3, matrix_a_values);
+        let matrix_b = Matrix::new(3, matrix_b_values);
+        let result_matrix = matrix_a.subtract_matrix(&matrix_b);
+        assert_eq!(result_matrix.columns_count, expected_result.columns_count);
+        assert_eq!(result_matrix.rows_count, expected_result.rows_count);
+        assert_eq!(result_matrix.values, expected_result.values);
+    }
+
+    #[test]
+    fn should_subtract_each_element_of_matrix_a_and_matrix_b_with_overloaded_subtract_operator() {
+        let matrix_a_values = vec![1.0, 2.0, 3.0];
+        let matrix_b_values = vec![3.0, 2.0, 1.0];
+        let expected_result = Matrix::new(3, vec![-2.0, 0.0, 2.0]);
+        let matrix_a = Matrix::new(3, matrix_a_values);
+        let matrix_b = Matrix::new(3, matrix_b_values);
+        let result_matrix = matrix_a - &matrix_b;
+        assert_eq!(result_matrix.columns_count, expected_result.columns_count);
+        assert_eq!(result_matrix.rows_count, expected_result.rows_count);
+        assert_eq!(result_matrix.values, expected_result.values);
+    }
+}
