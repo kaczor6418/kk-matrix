08416d0950396af4b2fd51d57bae223438e12b43
kaczor6418 <k.kaczynski97@gmail.com>
2021-02-20 00:27:38 +0100

    feat(matrix-subtract): add implementation for subtract operation

--- src/matrix/matrix/algebraic_operations.rs
+++ src/matrix/matrix/algebraic_operations.rs
@@ -2,6 +2,7 @@ use crate::matrix::matrix::Matrix;
 
 pub trait AlgebraicOperations {
     fn add_matrix(&self, matrix: &Matrix) -> Matrix;
+    fn subtract_matrix(&self, matrix: &Matrix) -> Matrix;
 }
 
 impl AlgebraicOperations for Matrix {
@@ -12,6 +13,17 @@ impl AlgebraicOperations for Matrix {
             self.values
                 .iter()
                 .map(|value| value + matrix_iter.next().unwrap())
+                .collect(),
+        );
+    }
+
+    fn subtract_matrix(&self, matrix: &Matrix) -> Matrix {
+        let mut matrix_iter = matrix.values.iter();
+        return Matrix::new(
+            matrix.columns_count,
+            self.values
+                .iter()
+                .map(|value| value - matrix_iter.next().unwrap())
                 .collect(),
         );
     }
