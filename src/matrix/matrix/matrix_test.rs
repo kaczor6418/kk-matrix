c715bd57f3352d2625b5a998c5dfda5aa77eecca
kaczor6418 <k.kaczynski97@gmail.com>
2021-02-20 00:42:23 +0100

    test(matrix-multiplication): add different cases of matrix multiplication

--- src/matrix/matrix/matrix_test.rs
+++ src/matrix/matrix/matrix_test.rs
@@ -292,3 +292,88 @@ mod multiply_by_digit {
         assert_eq!(result_matrix.values, expected_result.values);
     }
 }
+
+mod multiply_by_vector {
+    use crate::matrix::matrix::Matrix;
+
+    #[test]
+    fn should_multiply_matrix_values_by_vector_values() {
+        let vector = vec![1.0, 2.0, 3.0];
+        let matrix_values = vec![1.0, 2.0, 3.0];
+        let expected_result = Matrix::new(3, vec![1.0, 4.0, 9.0]);
+        let matrix = Matrix::new(3, matrix_values);
+        let result_matrix = matrix.multiply_by_vector(&vector);
+        assert_eq!(result_matrix.columns_count, expected_result.columns_count);
+        assert_eq!(result_matrix.rows_count, expected_result.rows_count);
+        assert_eq!(result_matrix.values, expected_result.values);
+    }
+
+    #[test]
+    fn should_multiply_matrix_values_by_vector_values_with_overloaded_mul_right_operator() {
+        let vector = vec![1.0, 2.0, 3.0];
+        let matrix_values = vec![1.0, 2.0, 3.0];
+        let expected_result = Matrix::new(3, vec![1.0, 4.0, 9.0]);
+        let matrix = Matrix::new(3, matrix_values);
+        let result_matrix = matrix * &vector;
+        assert_eq!(result_matrix.columns_count, expected_result.columns_count);
+        assert_eq!(result_matrix.rows_count, expected_result.rows_count);
+        assert_eq!(result_matrix.values, expected_result.values);
+    }
+
+    #[test]
+    fn should_multiply_matrix_values_by_vector_values_with_overloaded_mul_left_operator() {
+        let vector = vec![1.0, 2.0, 3.0];
+        let matrix_values = vec![1.0, 2.0, 3.0];
+        let expected_result = Matrix::new(3, vec![1.0, 4.0, 9.0]);
+        let matrix = Matrix::new(3, matrix_values);
+        let result_matrix = vector * &matrix;
+        assert_eq!(result_matrix.columns_count, expected_result.columns_count);
+        assert_eq!(result_matrix.rows_count, expected_result.rows_count);
+        assert_eq!(result_matrix.values, expected_result.values);
+    }
+}
+
+mod multiply_by_matrix {
+    use crate::matrix::matrix::Matrix;
+
+    #[test]
+    fn should_multiply_matrix_with_many_rows_and_columns_if_matrix_rows_count_equal_target_matrix_columns_count(
+    ) {
+        let matrix_a_values = vec![1.0, 1.0, 1.0, 2.0, 2.0, 2.0];
+        let matrix_b_values = vec![2.0, 2.0, 2.0, 3.0, 3.0, 3.0];
+        let expected_result = Matrix::new(2, vec![7.0, 8.0, 14.0, 16.0]);
+        let matrix_a = Matrix::new(3, matrix_a_values);
+        let matrix_b = Matrix::new(2, matrix_b_values);
+        let result_matrix = matrix_a.multiply_by_matrix(&matrix_b);
+        assert_eq!(result_matrix.columns_count, expected_result.columns_count);
+        assert_eq!(result_matrix.rows_count, expected_result.rows_count);
+        assert_eq!(result_matrix.values, expected_result.values);
+    }
+
+    #[test]
+    fn should_multiply_matrix_with_many_rows_and_columns_if_matrix_rows_count_equal_target_matrix_columns_count_with_overloaded_nul_operator(
+    ) {
+        let matrix_a_values = vec![1.0, 1.0, 1.0, 2.0, 2.0, 2.0];
+        let matrix_b_values = vec![2.0, 2.0, 2.0, 3.0, 3.0, 3.0];
+        let expected_result = Matrix::new(2, vec![7.0, 8.0, 14.0, 16.0]);
+        let matrix_a = Matrix::new(3, matrix_a_values);
+        let matrix_b = Matrix::new(2, matrix_b_values);
+        let result_matrix = matrix_a * &matrix_b;
+        assert_eq!(result_matrix.columns_count, expected_result.columns_count);
+        assert_eq!(result_matrix.rows_count, expected_result.rows_count);
+        assert_eq!(result_matrix.values, expected_result.values);
+    }
+
+    #[test]
+    fn should_multiply_matrix_with_one_row_if_target_matrix_has_one_column() {
+        let matrix_a_values = vec![1.0, 2.0, 3.0];
+        let matrix_b_values = vec![1.0, 2.0, 3.0];
+        let expected_result = Matrix::new(1, vec![14.0]);
+        let matrix_a = Matrix::new(3, matrix_a_values);
+        let matrix_b = Matrix::new(1, matrix_b_values);
+        let result_matrix = matrix_a.multiply_by_matrix(&matrix_b);
+        assert_eq!(result_matrix.columns_count, expected_result.columns_count);
+        assert_eq!(result_matrix.rows_count, expected_result.rows_count);
+        assert_eq!(result_matrix.values, expected_result.values);
+    }
+}
